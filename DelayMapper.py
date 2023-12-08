e4703c683568bd847dc93f098498fce4e65157dc
Muhammad Tukhtasunov <70448925+MuhammadTukhtasunov@users.noreply.github.com>
2023-12-07 20:21:15 -0500

    Update DelayMapper.py

--- DelayMapper.py
+++ DelayMapper.py
@@ -4,13 +4,16 @@ import sys
 
 for line in sys.stdin:
     line = line.strip()
-    fields = line.split(",")
+    unpacked = line.split(",")
+    Year, Month, DayofMonth, DayOfWeek, DepTime, CRSDepTime, ArrTime, CRSArrTime, UniqueCarrier, FlightNum, TailNum, ActualElapsedTime, CRSElapsedTime, AirTime, ArrDelay, DepDelay, Origin, Dest, Distance, TaxiIn,TaxiOut, Cancelled, CancellationCode, Diverted, CarrierDelay, WeatherDelay,  NASDelay, SecurityDelay, LateAircraftDelay = line.split(",")
+    # results = [DayOfWeek, DepDelay]
+    # fields = line.split(",")
 
-    # Extract relevant columns
-    UniqueCarrier = fields[8].strip()
-    Origin = fields[16].strip()
-    TaxiIn = float(fields[19].strip())
-    ArrDelay = float(fields[14].strip())
+    # # Extract relevant columns
+    # UniqueCarrier = fields[8].strip()
+    # Origin = fields[16].strip()
+    # TaxiIn = float(fields[19].strip())
+    # ArrDelay = float(fields[14].strip())
     # Ignore rows with missing values
     if 'NA' in [UniqueCarrier, Origin, TaxiIn, ArrDelay]:
         continue
@@ -18,3 +21,7 @@ for line in sys.stdin:
     key = "-".join([UniqueCarrier, Origin])
     # Emit key-value pairs
     print(f"{key}\t{TaxiIn + ArrDelay}")
+
+
+
+
