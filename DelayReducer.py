e5b71f3ddbdb06a7d407cbc2edf92f2d90b90aa5
Muhammad Tukhtasunov <70448925+MuhammadTukhtasunov@users.noreply.github.com>
2023-12-07 20:20:55 -0500

    Update DelayReducer.py

--- DelayReducer.py
+++ DelayReducer.py
@@ -1,40 +1,66 @@
-#!/usr/bin/env python
 
+
+#! /usr/bin/env python
 import sys
-
-current_key = None
+last_key = None
+count = 0
 min_value = float('inf')
 max_value = float('-inf')
-total_value = 0
-count = 0
+# total_value = 0
+sum = 1
 
+
+
+# keys come grouped together so we need to keep track of state a little bit  thus when the key changes, 
+# we need to reset our counter, and write out the count we've accumulated
 for line in sys.stdin:
-    line = line.strip()
-    key, value = line.split("\t")
+   line = line.strip()
+   key, value = line.split("\t")
 
-    # Convert value to float
-    value = float(value)
+   # we have to be able to deal with missing values
+   if value =="NA":
+       continue
+   
+   value = float(value)
 
-    # Update values for the current key
-    if current_key == key:
+   # if this is the first iteration
+   if not last_key:
+       last_key = key
+       min_value = min(min_value, value)
+       max_value = max(max_value, value)
+       count = 1
+       sum = value
+
+    # if they're the same, log it
+   elif key == last_key:
         min_value = min(min_value, value)
         max_value = max(max_value, value)
-        total_value += value
-        count += 1
-    else:
-        # Output results for the previous key
-        if current_key:
-            average_value = total_value / count
-            print(f"{current_key}\t{min_value}\t{max_value}\t{average_value}")
+        count = count + 1
+        sum = sum + value
+        
+   else: 
+        min_value = min(min_value, value)
+        max_value = max(max_value, value)
+        average_value = sum / count
+        print(f"{last_key}\t{min_value}\t{max_value}\t{average_value}")
 
-        # Reset values for the new key
-        current_key = key
-        min_value = value
-        max_value = value
-        total_value = value
+        last_key = key
         count = 1
+        sum = value
+        min_value = float('inf')
+        max_value = float('-inf')
+        
 
-# Output the results for the last key
-if current_key:
-    average_value = total_value / count
-    print(f"{current_key}\t{min_value}\t{max_value}\t{average_value}")
+# this is to catch the final value that we output
+min_value = min(min_value, value)
+max_value = max(max_value, value)
+average_value = sum / count
+print(f"{last_key}\t{min_value}\t{max_value}\t{average_value}")
+
+# Print the average for the last day
+if last_key is not None:
+    min_value = min(min_value, value)
+    max_value = max(max_value, value)
+    average_value = sum / count
+    print(f"{last_key}\t{min_value}\t{max_value}\t{average_value}")
+  
